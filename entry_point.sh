0fc1586cebafa95edfb55c9489fb3599a9d79fb0
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-18 23:37:29 -0500

    Update the C-Linter action for debug.

--- entry_point.sh
+++ entry_point.sh
@@ -1,17 +1,52 @@
 #!/usr/bin/env bash
+set -e
 
-whoami || true
-pwd || true
-ls -lha . || true
-echo "GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}" || true
-ls -lha "${GITHUB_EVENT_PATH}/files" || true
-ls -lha /home/ || true
-ls -lha /home/runner || true
+echo "=== Environment variables ==="
+echo "INPUT_PROJECT_PATH:     ${INPUT_PROJECT_PATH}"
+echo "INPUT_CPPCHECK_OPTIONS: ${INPUT_CPPCHECK_OPTIONS}"
+echo ""
+echo "=== Current location ==="
+pwd
+echo ""
+echo "=== List this directory ==="
+ls -lha .
+echo ""
+echo "=== Print Environment variables ==="
+printenv
+
+echo ""
+echo "=== Get C/C++ files ==="
+cd "${INPUT_PROJECT_PATH}"
+# *.c *.h *.cpp *.hpp *.cc *.c++ *.cp *.cxx
+FILES=$(find ./ -type f -regextype posix-extended -iregex '.*\.(c|h)?(\+\+|c|p|pp|xx)')
+echo "FILES: ${FILES}"
+
+echo ""
+echo "=== Performing checkup ==="
+clang-tidy --version
+for FILE in "${FILES[@]}"
+do
+   clang-tidy "${FILE}" -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* #>> clang-tidy-report.txt
+   clang-format --dry-run -Werror "${FILE}" || echo "File: $filename not formatted!" #>> clang-format-report.txt
+done
+cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx
+
+echo ""
+echo "=== Report: Tidy ==="
+cat clang-tidy-report.txt
+
+echo ""
+echo "=== Report: Format ==="
+cat clang-format-report.txt
+
+echo ""
+echo "=== Report: CPP Check ==="
+cat cppcheck-report.txt
 
 exit 123
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "The GITHUB_TOKEN is required."
+	echo "ERROR: The GITHUB_TOKEN is required."
 	exit 1
 fi
 
