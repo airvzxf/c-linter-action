4818665da3914d7af438b6d5fab36b3bbc01156a
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-19 09:28:07 -0500

    Update the C-Linter action for debug.

--- entry_point.sh
+++ entry_point.sh
@@ -21,7 +21,7 @@ if [ "${GITHUB_EVENT_NAME}" = "push" ]; then
   echo ""
   echo "=== Get C/C++ files ==="
   cd "${INPUT_PROJECT_PATH}" || (
-    echo "Error: The project path (${INPUT_PROJECT_PATH}) not exists."
+    echo "ERROR: The project path (${INPUT_PROJECT_PATH}) not exists."
     ls -lha "${INPUT_PROJECT_PATH}/.."
     exit 1
   )
@@ -70,21 +70,20 @@ if [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
   echo ""
   echo "=== GitHub Event: Pull request ==="
 
-  if [[ -z $GITHUB_TOKEN ]]; then
+  if [[ -z ${GITHUB_TOKEN} ]]; then
     echo "ERROR: The GITHUB_TOKEN is required."
     exit 1
   fi
 
-  # Add validation to check the GITHUB_EVENT_NAME is pull_request
-  FILES_LINK=$(jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH")/files
-  echo "Files = $FILES_LINK"
+  echo "=== Get pull request files ==="
+  FILES_LINK=$(jq -r '.pull_request._links.self.href' "${GITHUB_EVENT_PATH}")/files
+  echo "Files = ${FILES_LINK}"
 
-  curl "$FILES_LINK" > files.json
+  curl "${FILES_LINK}" > files.json
   FILES_URLS_STRING=$(jq -r '.[].raw_url' files.json)
 
-  readarray -t URLS <<< "$FILES_URLS_STRING"
+  readarray -t URLS <<< "${FILES_URLS_STRING}"
 
-  echo "File names: $URLS"
   echo "File names: " "${URLS[@]}"
 
   mkdir -p files
@@ -93,61 +92,81 @@ if [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
     ls -lha .
     exit 1
   )
-  for i in "${URLS[@]}"; do
-    echo "Downloading $i"
-    curl -LOk --remote-name $i
+
+  echo "=== Download pull request files ==="
+  for URL in "${URLS[@]}"; do
+    echo "Downloading ${URL}"
+    curl -LOk --remote-name "${URL}"
   done
+  ls -lha .
 
-  echo "Files downloaded!"
-  echo "Performing checkup:"
-  clang-tidy --version
-
-  # clang-format --style=llvm -i *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx > clang-format-report.txt
-  for i in "${URLS[@]}"; do
-    filename=$(basename $i)
-    clang-tidy $filename -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* >> clang-tidy-report.txt
-    clang-format --dry-run -Werror $filename || echo "File: $filename not formatted!" >> clang-format-report.txt
+  echo "=== Performing checkup ==="
+  for URL in "${URLS[@]}"; do
+    FILE_NAME=$(basename "${URL}")
+    clang-tidy "${FILE_NAME}" -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* >> clang-tidy-report.txt
+    clang-format --dry-run -Werror "${FILE_NAME}" || echo "File: ${FILE_NAME} not formatted!" >> clang-format-report.txt
   done
 
   cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx
 
+  echo "CLang Tidy style:"
+  clang-tidy --version
+  echo "-----------------"
+  clang-tidy --dump-config
+  echo "-----------------"
+  clang-tidy --list-checks
+  echo "-----------------"
+  clang-tidy --help-list-hidden
+  echo "-----------------"
+  clang-format --style=llvm -i *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx > clang-format-report-details.txt
+
   PAYLOAD_TIDY=$(cat clang-tidy-report.txt)
   PAYLOAD_FORMAT=$(cat clang-format-report.txt)
+  PAYLOAD_FORMAT_DETAILS=$(cat clang-format-report-details.txt)
   PAYLOAD_CPPCHECK=$(cat cppcheck-report.txt)
-  COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
+  COMMENTS_URL=$(cat "${GITHUB_EVENT_PATH}" | jq -r .pull_request.comments_url)
 
-  echo $COMMENTS_URL
+  echo "=== Display the reports ==="
+  echo "${COMMENTS_URL}"
   echo "Clang-tidy errors:"
-  echo $PAYLOAD_TIDY
+  echo "${PAYLOAD_TIDY}"
   echo "Clang-format errors:"
-  echo $PAYLOAD_FORMAT
+  echo "${PAYLOAD_FORMAT}"
+  echo "Clang-format details errors:"
+  echo "${PAYLOAD_FORMAT_DETAILS}"
   echo "Cppcheck errors:"
-  echo $PAYLOAD_CPPCHECK
+  echo "${PAYLOAD_CPPCHECK}"
 
-  if [ "$PAYLOAD_TIDY" != "" ]; then
+  echo "=== Generate the output ==="
+  if [ "${PAYLOAD_TIDY}" != "" ]; then
     OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
     OUTPUT+=$'\n```\n'
-    OUTPUT+="$PAYLOAD_TIDY"
+    OUTPUT+="${PAYLOAD_TIDY}"
     OUTPUT+=$'\n```\n'
   fi
 
-  if [ "$PAYLOAD_FORMAT" != "" ]; then
+  if [ "${PAYLOAD_FORMAT}" != "" ]; then
     OUTPUT=$'**CLANG-FORMAT WARNINGS**:\n'
     OUTPUT+=$'\n```\n'
-    OUTPUT+="$PAYLOAD_FORMAT"
+    OUTPUT+="${PAYLOAD_FORMAT}"
     OUTPUT+=$'\n```\n'
   fi
 
-  if [ "$PAYLOAD_CPPCHECK" != "" ]; then
+  if [ "${PAYLOAD_CPPCHECK}" != "" ]; then
     OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
     OUTPUT+=$'\n```\n'
-    OUTPUT+="$PAYLOAD_CPPCHECK"
+    OUTPUT+="${PAYLOAD_CPPCHECK}"
     OUTPUT+=$'\n```\n'
   fi
 
-  echo "OUTPUT is: \n $OUTPUT"
+  echo "OUTPUT is:"
+  echo "${OUTPUT}"
 
-  PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+  echo "=== Generate the payload ==="
+  PAYLOAD=$(echo '{}' | jq --arg body "${OUTPUT}" '.body = $body')
+  echo "PAYLOAD:"
+  echo "${PAYLOAD}"
 
-  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+  echo "=== Send the payload to GitHub API ==="
+  curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "${PAYLOAD}" "${COMMENTS_URL}"
 fi
