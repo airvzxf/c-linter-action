c74e0f502be4513506d37b0776fad42a1481fb17
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-18 22:31:39 -0500

    Updated the entry point file.

--- entry_point.sh
+++ entry_point.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -z "$GITHUB_TOKEN" ]; then
+if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "The GITHUB_TOKEN is required."
 	exit 1
 fi
@@ -26,11 +26,16 @@ done
 echo "Files downloaded!"
 echo "Performing checkup:"
 clang-tidy --version
-clang-tidy *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt
 
-clang-format --style=llvm -i *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx > clang-format-report.txt
+# clang-format --style=llvm -i *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx > clang-format-report.txt
+for i in "${URLS[@]}"
+do
+   filename=`basename $i`
+   clang-tidy $filename -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* >> clang-tidy-report.txt
+   clang-format --dry-run -Werror $filename || echo "File: $filename not formatted!" >> clang-format-report.txt
+done
 
-cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *
+cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx
 
 PAYLOAD_TIDY=`cat clang-tidy-report.txt`
 PAYLOAD_FORMAT=`cat clang-format-report.txt`
@@ -45,20 +50,28 @@ echo $PAYLOAD_FORMAT
 echo "Cppcheck errors:"
 echo $PAYLOAD_CPPCHECK
 
-OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_TIDY"
-OUTPUT+=$'\n```\n'
+if [ "$PAYLOAD_TIDY" != "" ]; then
+   OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
+   OUTPUT+=$'\n```\n'
+   OUTPUT+="$PAYLOAD_TIDY"
+   OUTPUT+=$'\n```\n'
+fi
 
-OUTPUT=$'**CLANG-FORMAT WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_FORMAT"eCTF20/mb/drm_audio_fw/src on
-OUTPUT+=$'\n```\n'
+if [ "$PAYLOAD_FORMAT" != "" ]; then
+   OUTPUT=$'**CLANG-FORMAT WARNINGS**:\n'
+   OUTPUT+=$'\n```\n'
+   OUTPUT+="$PAYLOAD_FORMAT"
+   OUTPUT+=$'\n```\n'
+fi
 
-OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_CPPCHECK"
-OUTPUT+=$'\n```\n'
+if [ "$PAYLOAD_CPPCHECK" != "" ]; then
+   OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
+   OUTPUT+=$'\n```\n'
+   OUTPUT+="$PAYLOAD_CPPCHECK"
+   OUTPUT+=$'\n```\n'
+fi
+
+echo "OUTPUT is: \n $OUTPUT"
 
 PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
 
