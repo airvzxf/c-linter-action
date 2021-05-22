7b1d2cd39b500a0bc52ff6784c19d0a8442de6d3
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-22 05:11:34 -0500

    Update C-Linter action for debug.

--- entry_point.sh
+++ entry_point.sh
@@ -50,6 +50,11 @@ if [[ ${GITHUB_EVENT_NAME} == "push" ]]; then
   echo "=== X2 ==="
   printenv
 
+  echo ""
+  echo "=== X3 ==="
+  ls -lha "GITHUB_ENV: ${GITHUB_ENV}"
+  ls -lha "GITHUB_EVENT_PATH: ${GITHUB_ENV}"
+
   # echo ""
   # echo "=== Get committed files in JSON format ==="
   # GITHUB_FILES_JSON=$(jq -r '.pull_request._links.self.href' "${GITHUB_EVENT_PATH}")/files
